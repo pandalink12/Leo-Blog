05d835eae5fab74847645cfbe86fb42dea02c851
Leo Wong <pandalink@gmail.com>
2021-01-09 21:13:04 +1100

    Web Server Gunicorn added

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, validators
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL, Length
 from flask_ckeditor import CKEditorField
 
@@ -15,7 +15,7 @@ class CreatePostForm(FlaskForm):
 
 # Register form
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), validators.Email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Sign Me Up!")
@@ -23,7 +23,7 @@ class RegisterForm(FlaskForm):
 
 # Login form
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), validators.Email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField("Sign Me Up!")
 
